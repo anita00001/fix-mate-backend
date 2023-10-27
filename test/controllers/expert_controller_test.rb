9bff42d73185b3f18d6d9e998dbc4a5fca9b4678
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-10-27 16:02:02 +0530

    Fix linters

--- test/controllers/expert_controller_test.rb
+++ test/controllers/expert_controller_test.rb
@@ -1,17 +1,17 @@
-require "test_helper"
+require 'test_helper'
 
 class ExpertControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get expert_index_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get expert_new_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get expert_show_url
     assert_response :success
   end
