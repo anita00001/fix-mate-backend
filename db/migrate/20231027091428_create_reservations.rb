dfe13b5e98315efa039363103a42a2d25b5d8245
Theodoraldo <theodoraldo@gmail.com>
2023-10-27 11:38:36 +0000

    Change db column for reservation

--- db/migrate/20231027091428_create_reservations.rb
+++ db/migrate/20231027091428_create_reservations.rb
@@ -1,8 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[7.1]
   def change
     create_table :reservations do |t|
-      t.string :expert_id
-      t.int :user_id
+      t.integer :expert_id
+      t.integer :user_id
       t.string :city
       t.date :date
 
--- db/migrate/20231027104229_change_column_type_in_reservations.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnTypeInReservations < ActiveRecord::Migration[7.1]
-  def change
-    change_column :reservations, :expert_id, :integer
-  end
-end
