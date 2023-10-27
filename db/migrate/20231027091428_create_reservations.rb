846d4df3862754e5fbdc3327823a76038160e8e8
Theodoraldo <theodoraldo@gmail.com>
2023-10-27 13:36:17 +0000

    Modify file to correct migration

--- db/migrate/20231027091428_create_reservations.rb
+++ db/migrate/20231027091428_create_reservations.rb
@@ -1,8 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[7.1]
   def change
     create_table :reservations do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :expert, null: false, foreign_key: true
+      t.references :users, null: false, foreign_key: true
+      t.references :experts, null: false, foreign_key: true
       t.string :city
       t.date :date
 
--- db/migrate/20231027092658_create_experts.rb
+++ db/migrate/20231027092658_create_experts.rb
@@ -5,7 +5,7 @@ class CreateExperts < ActiveRecord::Migration[7.1]
       t.text :last_name
       t.text :email
       t.text :address
-      t.references :specialization, null: false, foreign_key: true
+      t.references :specializations, null: false, foreign_key: true
       t.integer :experience
       t.boolean :status, default: true
       t.boolean :removed, default: false
