5b805b72d5fe7366bf3b9bb8dda6999f702f90fe
Theodoraldo <theodoraldo@gmail.com>
2023-10-27 10:06:34 +0000

    Add validations to Reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,6 @@
 class Reservation < ApplicationRecord
+  validates :expert_id, presence: true
+  validates :user_id, presence: true
+  validates :city, presence: true
+  validates :date, presence: true
 end
