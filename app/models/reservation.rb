09644811b0389a7b9375af17e7b72c9a94e598d5
Theodoraldo <theodoraldo@gmail.com>
2023-10-27 14:37:11 +0000

    Resolve assignment

--- app/models/reservation.rb
+++
@@ -1,9 +0,0 @@
-class Reservation < ApplicationRecord
-  validates :expert_id, presence: true
-  validates :user_id, presence: true
-  validates :city, presence: true
-  validates :date, presence: true
-
-  belongs_to :expert
-  belongs_to :user
-end
