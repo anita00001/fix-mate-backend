0414d77b7e591b9253d7919b63faa903a419abce
Theodoraldo <theodoraldo@gmail.com>
2023-10-27 09:25:48 +0000

    Add columns for reservation table

--- db/migrate/20231027091428_create_reservations.rb
+++ db/migrate/20231027091428_create_reservations.rb
@@ -1,6 +1,10 @@
 class CreateReservations < ActiveRecord::Migration[7.1]
   def change
     create_table :reservations do |t|
+      t.string :expert_id
+      t.int :user_id
+      t.string :city
+      t.date :date
 
       t.timestamps
     end
