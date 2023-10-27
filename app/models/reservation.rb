2a1d196cdfd6171648aa52b2ebf99b6717081c55
Theodoraldo <theodoraldo@gmail.com>
2023-10-27 10:09:19 +0000

    Add association to reservation table

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,7 @@ class Reservation < ApplicationRecord
   validates :user_id, presence: true
   validates :city, presence: true
   validates :date, presence: true
+
+  belongs_to :expert
+  belongs_to :user
 end
