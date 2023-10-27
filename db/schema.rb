494dbb7eaaf4fd98d6c14ccf12c3c06cee25113d
moFuhidy <mo7ammed9290@gmail.com>
2023-10-27 18:06:43 +0300

    Add foreign key constraint  ExpertSpecialization

--- db/schema.rb
+++ db/schema.rb
@@ -9,3 +9,40 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2023_10_27_145723) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "experts", force: :cascade do |t|
+    t.text "first_name"
+    t.text "last_name"
+    t.text "email"
+    t.text "address"
+    t.integer "experience"
+    t.boolean "status", default: true
+    t.boolean "removed", default: false
+    t.text "image"
+    t.decimal "fee"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "specialization_id", null: false
+    t.index ["specialization_id"], name: "index_experts_on_specialization_id"
+  end
+
+  create_table "specializations", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.text "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "experts", "specializations"
+end
