84e99ee3b14598c7cfdc33d9932e872c1baef3eb
Theodoraldo <theodoraldo@gmail.com>
2023-10-27 10:33:28 +0000

    Correct linter error

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -1,7 +1,7 @@
 class Expert < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :address, presence: true 
+  validates :address, presence: true
   validates :specialization_id, presence: true
   validates :experience, presence: true
   validates :image, presence: true
