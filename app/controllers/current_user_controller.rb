02665b1446c2725ba286a1a5c15b565352ab9204
Anita Sharma <sharma.anita00001@gmail.com>
2023-10-28 11:29:07 +0545

    Fix 'session cookies' bug

---
+++ app/controllers/current_user_controller.rb
@@ -0,0 +1,7 @@
+class CurrentUserController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    render json: UserSerialzier.new(current_user).serializeable_hash[:data][:attributes], status: :ok
+  end
+end
