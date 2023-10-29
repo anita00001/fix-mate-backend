24aaa81a7a6a2d085e44697f5dc27a3f6177e37e
Anita Sharma <sharma.anita00001@gmail.com>
2023-10-29 17:42:09 +0545

    Devise user model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,:jwt_authenticatable, jwt_revocation_strategy: self
+
+  def jwt_payload
+    super
+  end
+end
