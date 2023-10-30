598a4683b0b87a48b928b54c861045eec2729cb7
Theodoraldo <theodoraldo@gmail.com>
2023-10-30 05:07:11 +0000

    Modify experts controller

---
+++ app/controllers/experts_controller.rb
@@ -0,0 +1,33 @@
+class ExpertsController < ApplicationController
+  def index
+    @experts = Expert.includes(:specialization).where(removed: :true)
+
+    render json: @experts
+  end
+
+  def new
+    @expert = Expert.new
+  end
+
+  def show
+    @expert = Expert.find(params[:id])
+    @specialization = @expert.specialization
+
+    render json: @specialization
+  end
+
+  def toggle_remove
+    @expert = Expert.find(params[:id])
+    if @expert.change_removed_status
+      render json: { status: 'success', message: 'Toggle successful' }
+    else
+      render json: { status: 'error', message: 'Toggle failed' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:expert).permit(:first_name, :last_name, :email, :address, :experience, :status, :removed, :image, :fee)
+  end
+end
