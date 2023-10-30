4da34b468af3b13f285baa1b27fb6332dba7ef50
Theodoraldo <theodoraldo@gmail.com>
2023-10-30 13:32:45 +0000

    Resolve routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -10,20 +10,17 @@ Rails.application.routes.draw do
   }
   get "up" => "rails/health#show", as: :rails_health_check
 
-  namespace :api do
-    namespace :v1 do
-      resources :experts, only: [:index, :new, :create, :show ] do
-        member do
-          put :toggle_remove
-        end
-        resources :specializations, only: [:index,:new, :show]
-      end
+  resources :experts, only: [:index, :new, :create, :show ] do
+    member do
+      put :toggle_remove
     end
+    resources :specializations, only: [:index,:new, :show]
   end
 
   namespace :api do
     namespace :v1 do
       resources :reservations
+      resources :experts
     end
   end
 end
